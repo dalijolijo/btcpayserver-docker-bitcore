b585b59b0ea8a34f14ce2680552e0660509c8090
David B <david.bergen@gmx.net>
2018-11-16 17:20:02 +0100

    Add Ubuntu 16.04 Dockerfile

---
+++ btx-ubuntu/docker-entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+mkdir -p "$BITCORE_DATA"
+
+cat <<-EOF > "$BITCORE_DATA/bitcore.conf"
+printtoconsole=1
+rpcallowip=::/0
+${BITCOIN_EXTRA_ARGS}
+EOF
+chown bitcore:bitcore "$BITCORE_DATA/bitcore.conf"
+
+# ensure correct ownership and linking of data directory
+# we do not update group ownership here, in case users want to mount
+# a host directory and still retain access to it
+chown -R bitcore "$BITCORE_DATA"
+ln -sfn "$BITCORE_DATA" /home/bitcore/.bitcore
+chown -h bitcore:bitcore /home/bitcore/.bitcore
+	
+# Downloading bootstrap file
+WEB="bitcore.cc" # without "https://" and without the last "/" (only HTTPS accepted)
+BOOTSTRAP="bootstrap.tar.gz"
+cd $BITCORE_DATA
+rm -rf ${BOOTSTRAP}
+if [ "$(curl -Is https://${WEB}/${BOOTSTRAP} | head -n 1 | tr -d '\r\n')" = "HTTP/1.1 200 OK" ] ; then \
+       	sudo -u bitcore wget https://${WEB}/${BOOTSTRAP}; \
+       	sudo -u bitcore tar -xvzf ${BOOTSTRAP}; \
+       	sudo -u bitcore rm ${BOOTSTRAP}; \
+fi
+
+exec gosu bitcore "$@"
